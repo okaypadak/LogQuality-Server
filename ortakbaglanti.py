a0e6d3a80b4c7ac9e3137555ab65a125c8e5ea4f
Okay Padak <okaypadak@github.com>
2023-11-17 16:39:12 +0300

    ortak transaction

--- ortakbaglanti.py
+++ ortakbaglanti.py
@@ -1,5 +1,31 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.pool import QueuePool
 
-engine = create_engine('postgresql://admin:1234@localhost:5432/logstrack')
+db_url = 'postgresql://admin:1234@localhost:5432/logstrack'
+engine = create_engine(db_url, poolclass=QueuePool, pool_size=5, max_overflow=10)
 Base = declarative_base()
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine, autocommit=True)
+session = Session()
+
+
+SessionScope = scoped_session(sessionmaker(bind=engine))
+
+@contextmanager
+def transaction_scope():
+    session = Session()
+
+    try:
+        yield session
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        raise e
+    finally:
+        Session.remove()
--- takip.py
+++ takip.py
@@ -1,6 +1,8 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine, Column, String, Date, Integer
 import ortakbaglanti as baglanti
-from sqlalchemy.orm import sessionmaker
+
 
 class takip(baglanti.Base):
     __tablename__ = 'takip'
@@ -10,30 +12,27 @@ class takip(baglanti.Base):
     metod = Column(String(collation='default'))
     hata = Column(String(collation='default'))
 
-baglanti.Base.metadata.create_all(baglanti.engine)
-
-Session = sessionmaker(bind=baglanti.engine, autocommit=True)
-session = Session()
-
 def create_entry(date, class_name, method, error):
     new_entry = takip(tarih=date, sinif=class_name, metod=method, hata=error)
-    session.add(new_entry)
-    #session.commit()
+    baglanti.session.add(new_entry)
+    #session.commit()şim
     print(f"Yeni eklenen girişin ID'si: {new_entry.id}")
 
 def read_all_entries():
-    all_entries = session.query(takip).all()
+    all_entries = baglanti.session.query(takip).all()
     for entry in all_entries:
         print(f"ID: {entry.id}, Tarih: {entry.tarih}, Sınıf: {entry.sinif}, Metod: {entry.metod}, Hata: {entry.hata}")
 
 def update_entry(entry_id, new_error):
-    entry_to_update = session.query(takip).filter_by(id=entry_id).first()
+    entry_to_update = baglanti.session.query(takip).filter_by(id=entry_id).first()
     if entry_to_update:
         entry_to_update.hata = new_error
         #session.commit()
 
 def delete_entry(entry_id):
-    entry_to_delete = session.query(takip).filter_by(id=entry_id).first()
+    entry_to_delete = baglanti.session.query(takip).filter_by(id=entry_id).first()
     if entry_to_delete:
-        session.delete(entry_to_delete)
+        baglanti.session.delete(entry_to_delete)
         #session.commit()
+
+
