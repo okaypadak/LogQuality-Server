90a6df0b0917f69f537f1863395992a4adf5db4c
kypdk@proton.me <kypdk@proton.me>
2023-12-16 20:43:38 +0300

    yeni sutunlar ve Proje belli bir hash_id ile kayıt olsun

--- repository/Proje.py
+++ repository/Proje.py
@@ -1,7 +1,9 @@
+import base64
+
 from models.ProjeModel import Proje
 from models.GunlukSayacModel import GunlukSayac
-
-
+import hashlib
+import time
 
 # CRUD işlemleri için fonksiyonlar
 from datetime import datetime
@@ -12,7 +14,7 @@ from repository.Takip import TakipManager
 
 class ProjeManager:
 
-    def create_proje(self, session, proje_adi, sunucu_ip, sunucu_port, kullanici_adi, kullanici_sifre, log_dosya_yolu, secim):
+    def create_proje(self, session, proje_adi, sunucu_ip, sunucu_port, kullanici_adi, kullanici_sifre, log_dosya_yolu, secim, git_url):
         new_proje = Proje(
             proje_adi=proje_adi,
             sunucu_ip=sunucu_ip,
@@ -20,18 +22,20 @@ class ProjeManager:
             kullanici_adi=kullanici_adi,
             kullanici_sifre=kullanici_sifre,
             log_dosya_yolu=log_dosya_yolu,
-            secim=secim
+            secim=secim,
+            git_url=git_url,
+            hash_id=self.generate_short_hash_id(),
+            aktif=1
         )
 
         session.add(new_proje)
         session.push()
-        #session.refresh(new_proje)
+        # session.refresh(new_proje)
 
         return new_proje
 
     def get_all_projeler(self, session):
         return session.query(Proje).all()
-
 
     def get_proje_by_id(self, session, proje_id):
         return session.query(Proje).filter(Proje.id == proje_id).first()
@@ -46,7 +50,6 @@ class ProjeManager:
 
         for proje in projeler:
             gunlukSayacManager.control_and_create(session, proje.id, today_date)
-
 
         gelen_projeler = (
             session.query(Proje, GunlukSayac)
@@ -95,3 +98,16 @@ class ProjeManager:
         session.commit()
         session.refresh(new_gunluk_sayac)
         return new_gunluk_sayac
+
+    def generate_short_hash_id(self):
+        # Zaman damgasını kullanarak benzersiz bir veri oluştur
+        unique_data = str(time.time())
+
+        # SHA-256 hash fonksiyonu kullanarak hash değerini oluştur
+        hash_object = hashlib.sha256(unique_data.encode())
+        hash_bytes = hash_object.digest()
+
+        # Base64 formatına çevir
+        hash_id = base64.urlsafe_b64encode(hash_bytes).decode('utf-8')[:10]
+
+        return hash_id
