7d12b8c5fbfa642a0836eaf8b1cdb8988e6a7c97
kypdk@proton.me <kypdk@proton.me>
2024-03-05 17:49:26 +0300

    log akışında işlenmemiş ve WARN seviyesindeki logları yakalayan daha sonra işlenmiş olarak günceller

--- reader/ElasticSearchReader.py
+++ reader/ElasticSearchReader.py
@@ -1,22 +1,35 @@
-
 import time
 from concurrent.futures import as_completed, ThreadPoolExecutor
 from datetime import datetime
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, NotFoundError
 from elasticsearch.helpers import scan
 from reader.ProjectList import project
 from util.LogProcess import logger
-
+import queue
 
 class ElasticSearchReader:
     def __init__(self):
         self.es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
+        self.log_queue = queue.Queue()
 
+    def ayristir(self, proje_id, logs):
+        global doc_id
+        for log in logs:
+            logSource = log['_source']
+            if logSource['level'] == 'WARN' and logSource.get('processed', False) is not True:
+                logger.info(f"Warning log detected: {log}")
+                try:
+
+                    doc_id = log['_id']
+                    doc = self.es.get(index=log['_index'], id=doc_id)
+
+                    doc['_source']['processed'] = True
+                    self.es.update(index=log['_index'], id=doc_id, body={'doc': doc['_source']})
+                except NotFoundError:
+                    logger.error(f"Document with ID {doc_id} not found.")
 
     def streaming(self, proje):
-
         while True:
-            
             today = datetime.now().strftime("%Y-%m-%d")
             start_of_day = today + "T00:00:00"
             end_of_day = today + "T23:59:59"
@@ -32,31 +45,23 @@ class ElasticSearchReader:
                 }
             }
 
-            logs = []
-
-
-            logs = scan(self.es, query=query, index="testapplogs")
+            logs = scan(self.es, query=query, index=proje['index_name'])
 
             for log in logs:
-                print(log['_source'])
+                self.log_queue.put((proje['proje_id'], log))
 
             time.sleep(1)
 
-
+    def process_logs(self):
+        while True:
+            if not self.log_queue.empty():
+                proje_id, log = self.log_queue.get()
+                self.ayristir(proje_id, [log])
+            else:
+                time.sleep(1)
 
     def start(self):
-
         with ThreadPoolExecutor() as executor:
             futures = {executor.submit(self.streaming, proje): proje for proje in project.list()}
+            executor.submit(self.process_logs)
 
-            for future in as_completed(futures):
-                try:
-                    if future.exception() is None:  # Hata yoksa
-                        proje_id, logs = future.result()
-
-                        if logs is not None:
-                            logger.info("geldi")
-                            # self.ayristir(proje_id, lines)
-
-                except Exception as e:
-                    logger.error(f"Hata: {e}")
